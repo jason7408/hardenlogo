555a5d7505b0756bd1c06b8f3c56bd120d7cb8d6
amity871028 <amity3744039@gmail.com>
2019-09-26 20:15:12 +0800

    Initial Commit

---
+++ hardenlogo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hardenlogo
+//
+//  Created by User04 on 2019/9/26.
+//  Copyright © 2019 jason7408. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
